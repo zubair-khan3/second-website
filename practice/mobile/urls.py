668e41a9afc4148775e20013ec116eb1982510fb
zubair <zubair.khan3@outlook.com>
2021-04-24 18:08:20 +0530

    updated punctuation function properly working

--- practice/mobile/urls.py
+++ practice/mobile/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.mobile),
+    path('', views.index),
+    path('analyze',views.analyze),
 ]
--- practice/mobile/views.py
+++ practice/mobile/views.py
@@ -4,6 +4,20 @@ import datetime
 
 
 # Create your views here.
-def mobile(request):
-    num = {'num': '23'}
-    return render(request, 'index.html', num)
+def index(request):
+    return render(request, 'index.html')
+
+
+def analyze(request):
+    text = request.GET.get('text', 'default')
+    print(text, "this is main text")
+    final = text
+    print(final, "this is final ")
+    punc1 = ''' !@#$%^&*()_+{}[]|\:;"'<,>`.?/~'''
+    result = ""
+    for words in final:
+        print(words, "this is word variable")
+        if words not in punc1:
+            result = result + words
+            print(result, "this is result")
+    return render(request, 'result.html', {'result': result, 'usertext': text})
